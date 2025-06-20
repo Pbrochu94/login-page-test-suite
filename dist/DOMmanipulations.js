361056330d519cadea6e8bb2ea342301a0c17931
Patrick Brochu <patrickbrochu70@gmail.com>
2025-06-20 16:24:53 -0400

    password and mail validation + message pop up completed

--- dist/DOMmanipulations.js
+++ dist/DOMmanipulations.js
@@ -1,24 +1,44 @@
 export { login };
 let login = {
     mailSelector: () => {
-        let selector = document.querySelector(`.email-input`);
-        return selector;
+        return document.querySelector(`.email-input`);
+    },
+    mailHelperSelector: () => {
+        return document.querySelector(`.error-message-mail`);
     },
     passSelector: () => {
-        let selector = document.querySelector(`[name="password"]`);
-        return selector;
+        return document.querySelector(`[name="password"]`);
+    },
+    passHelperSelector: () => {
+        return document.querySelector(`.error-message-password`);
     },
     credErrMess: (invalidInput) => {
         if (invalidInput === `email`) {
             let mailInput = login.mailSelector();
             if (mailInput) {
                 mailInput.style.border = "2px solid rgb(102, 19, 33)";
+                login.showCredHelper(`email`);
             }
         }
         else {
             let passInput = login.passSelector();
             if (passInput) {
                 passInput.style.border = "2px solid rgb(102, 19, 33)";
+                login.showCredHelper("password");
+            }
+        }
+    },
+    showCredHelper: (invalidInput) => {
+        if (invalidInput === `email`) {
+            let mailHelper = login.mailHelperSelector();
+            if (mailHelper) {
+                mailHelper.classList.remove(`hidden`);
+            }
+        }
+        if (invalidInput === `password`) {
+            let passHelper = login.passHelperSelector();
+            if (passHelper) {
+                passHelper.classList.remove(`hidden`);
             }
         }
     },
--- src/DOMmanipulations.ts
+++ src/DOMmanipulations.ts
@@ -3,28 +3,49 @@ export { login };
 let login: {
   mailSelector: () => HTMLInputElement | null;
   passSelector: () => HTMLInputElement | null;
+  mailHelperSelector: () => HTMLInputElement | null;
+  passHelperSelector: () => HTMLInputElement | null;
   credErrMess: (value: string) => void;
+  showCredHelper: (input: string) => void;
 } = {
   mailSelector: () => {
-    let selector: HTMLInputElement | null =
-      document.querySelector(`.email-input`);
-    return selector;
+    return document.querySelector(`.email-input`);
+  },
+  mailHelperSelector: () => {
+    return document.querySelector(`.error-message-mail`);
   },
   passSelector: () => {
-    let selector: HTMLInputElement | null =
-      document.querySelector(`[name="password"]`);
-    return selector;
+    return document.querySelector(`[name="password"]`);
+  },
+  passHelperSelector: () => {
+    return document.querySelector(`.error-message-password`);
   },
   credErrMess: (invalidInput: string) => {
     if (invalidInput === `email`) {
       let mailInput: HTMLInputElement | null = login.mailSelector();
       if (mailInput) {
         mailInput.style.border = "2px solid rgb(102, 19, 33)";
+        login.showCredHelper(`email`);
       }
     } else {
       let passInput: HTMLInputElement | null = login.passSelector();
       if (passInput) {
         passInput.style.border = "2px solid rgb(102, 19, 33)";
+        login.showCredHelper("password");
+      }
+    }
+  },
+  showCredHelper: (invalidInput) => {
+    if (invalidInput === `email`) {
+      let mailHelper: HTMLInputElement | null = login.mailHelperSelector();
+      if (mailHelper) {
+        mailHelper.classList.remove(`hidden`);
+      }
+    }
+    if (invalidInput === `password`) {
+      let passHelper: HTMLInputElement | null = login.passHelperSelector();
+      if (passHelper) {
+        passHelper.classList.remove(`hidden`);
       }
     }
   },
